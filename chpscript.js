80207291cea4f7d2a9483d828fd6c6fee1ab93ed
seikhchilli <saurabhkrsingh2001@gmail.com>
2022-01-10 17:22:07 +0530

    added submit button

---
+++ chpscript.js
@@ -0,0 +1,84 @@
+const header = document.querySelector('header');
+const ol = document.querySelector('ol');
+const form = document.querySelector('form');
+
+let requestURL = 'https://seikhchilli.github.io/indainPolitydb/';
+
+let request = new XMLHttpRequest();
+
+request.open('GET', requestURL);
+
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    const chapters = request.response;
+    populateHeader(chapters);
+    populateOl(chapters);
+}
+
+function populateHeader(obj){
+    const h1 = document.createElement('h1');
+    h1.textContent = obj['chapters'][0]['name'];
+
+    header.appendChild(h1);
+}
+
+function populateOl(obj){
+    const chp = obj['chapters'][0]['questions'];
+    for (let i = 0; i < chp.length; i++){
+        const li = document.createElement('li');
+
+        const p = document.createElement('p');
+        p.innerHTML = chp[i]['q'].join('<br><br>');
+
+        li.appendChild(p);
+        for(let j = 0; j < 4; j++){
+            var chr = String.fromCharCode(97 + j);
+
+            const inp = document.createElement('input');
+            const label = document.createElement('label');
+
+            inp.type = 'radio';
+            inp.id = chr;
+            inp.value = chr;
+            inp.name = i.toString();
+
+            label.for = chr;
+            label.innerHTML = chp[i][chr] + '<br>';
+
+            li.appendChild(inp);
+            li.appendChild(label);
+
+        }
+
+        ol.appendChild(li);
+    }
+
+}
+var section = document.querySelector('section');
+var btn1 = document.querySelector('button');
+
+form.addEventListener("submit", function(event) {
+    const db = request.response;
+    var ques = db['chapters'][0]['questions'];
+    var data = new FormData(form);
+    var result = 0;
+    for (const entry of data) {
+        if(entry[1] == ques[entry[0]]['ra']){
+            result += 1;
+        }
+    };
+    btn1.remove();
+    var div2 = document.createElement('div');
+    div2.innerText = "Result: " + result.toString();
+    div2.className = "res";
+
+    section.appendChild(div2);
+    
+    event.preventDefault();
+}, false);
+
+  
+
+
