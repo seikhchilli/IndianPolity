35dde7720dc7f8cc26b917d2c00f5bcc6cae9204
seikhchilli <saurabhkrsingh2001@gmail.com>
2022-02-02 12:54:10 +0530

    changed result line

--- resultScript.js
+++ resultScript.js
@@ -3,6 +3,6 @@ const main = document.querySelector('main');
 var params = location.search.split('?')[1];
 var result = params.split('=')[1];
 
-main.textContent = "Result: " + result;
+main.textContent = "Correct: " + result.split('/')[0] + " out of " + result.split('/')[1];
 
 
